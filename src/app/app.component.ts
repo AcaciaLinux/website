ff94c90b199d51bbcbea91261fcdafe93408cd43
Max Kofler <kofler.max.dev@gmail.com>
2023-01-31 17:51:54 +0100

    Added navbar to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  navbarHeight = 0;
   title = 'website';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.navbarHeight = this.el.nativeElement.querySelector('#navbar').offsetHeight;
+  }
+
+  calcHeight() {
+    return window.innerHeight - this.navbarHeight;
+  }
 }
