0ddd03e8f9328e971ceb74275d3cbf2d407dfa02
Max Kofler <kofler.max.dev@gmail.com>
2023-01-31 21:05:21 +0100

    Implemented BuildJobsComponent

--- src/app/admin/build-jobs/build-jobs.component.ts
+++ src/app/admin/build-jobs/build-jobs.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { BranchService } from 'src/app/shared/branch/branch.service';
+import { Job } from 'src/app/shared/classes/job';
 
 @Component({
   selector: 'app-build-jobs',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class BuildJobsComponent {
 
+  public queued_jobs?: Job[];
+  public running_jobs?: Job[];
+  public completed_jobs?: Job[];
+  subscription?: Subscription;
+
+  constructor(public branch: BranchService){
+
+  }
+
+  ngOnInit() {
+    const source = interval(5000);
+    this.subscription = source.subscribe(val => this.updateData());
+    this.updateData();
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe;
+  }
+
+  updateData(){
+    this.branch.request("joblist")
+      .subscribe(data => {
+        console.log("Updating data");
+        this.queued_jobs = data.payload.queued_jobs;
+        this.running_jobs = data.payload.running_jobs;
+        this.completed_jobs = data.payload.completed_jobs;
+      })
+  }
 }
---
+++ src/app/shared/classes/job.ts
@@ -0,0 +1,6 @@
+export class Job{
+	public build_pkg_name: string = ""
+	public job_id: string = ""
+	public requesting_client: string = ""
+	public job_status: string = ""
+}
