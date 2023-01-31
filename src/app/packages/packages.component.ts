3d6141db7f02ba56588e34f50deb1bad675de67a
Max Kofler <kofler.max.dev@gmail.com>
2023-01-31 20:19:31 +0100

    Added Packages table

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BranchService } from '../shared/branch/branch.service';
+import { map } from 'rxjs/operators';
+import { Package } from '../shared/classes/package';
 
 @Component({
   selector: 'app-packages',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./packages.component.css']
 })
 export class PackagesComponent {
+
+  public packages?: Package[];
+
+  constructor(public branch: BranchService){
+
+  }
+
+  ngOnInit() {
+    this.branch.request("packagelist")
+      .subscribe(data => {
+        this.packages = data.payload; //Create the packages array
+        this.packages?.sort((a, b) => a.name?.localeCompare(b.name)) //Sort the packages by name
+      })
+  }
+
+  getPkgDepsString(pkg: Package): string{
+    let res = "";
+    if (pkg.dependencies === undefined)
+      return res;
+
+    for (let dep of pkg.dependencies){
+      res += dep + " ";
+    }
+
+    return res;
+  }
 
 }
