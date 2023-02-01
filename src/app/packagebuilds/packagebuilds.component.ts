78f80a821006b45dbac0d8b7097be2baf71cb73e
Max Kofler <kofler.max.dev@gmail.com>
2023-02-01 11:46:11 +0100

    Implemented PackageBuilds

--- src/app/packagebuilds/packagebuilds.component.ts
+++ src/app/packagebuilds/packagebuilds.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { BranchService } from '../shared/branch/branch.service';
+import { Package } from '../shared/classes/package';
 
 @Component({
   selector: 'app-packagebuilds',
@@ -6,5 +9,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./packagebuilds.component.css']
 })
 export class PackagebuildsComponent {
+  public pkgbuilds?: string[];
+  public packages?: Package[];
+  subscription?: Subscription;
 
+  constructor(public branch: BranchService){
+
+  }
+
+  ngOnInit() {
+    const source = interval(15000);
+    this.subscription = source.subscribe(val => this.updateData());
+    this.updateData();
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe;
+  }
+
+  pkgIsBuilt(name: string): boolean{
+    let pkg = this.packages?.find(p => p.name == name);
+    return pkg !== undefined;
+  }
+
+  getVersionByPkgName(name: string): string{
+    let pkg = this.packages?.find(p => p.name == name);
+    if (pkg === undefined){
+      return "NOT BUILT";
+    }
+  
+    return pkg.version;
+  }
+
+  getRelVersionByPkgName(name: string): string{
+    let pkg = this.packages?.find(p => p.name == name);
+    if (pkg === undefined){
+      return "NOT BUILT";
+    }
+  
+    return pkg.real_version.toString();
+  }
+
+  updateData(){
+    this.branch.request("packagebuildlist")
+      .subscribe(data => {
+        console.log("Updating data");
+        this.pkgbuilds = data.payload;
+      })
+    this.branch.request("packagelist")
+      .subscribe(data => {
+        console.log("Updating data");
+        this.packages = data.payload;
+      })
+  }
 }
