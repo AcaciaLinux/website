cc26858fc168f4c131dced4ddddf24ea60a9b28b
Max Kofler <kofler.max.dev@gmail.com>
2023-02-02 11:38:37 +0100

    Updated nav setup to be dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,4 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  navbarHeight = 0;
-  title = 'website';
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    this.navbarHeight = this.el.nativeElement.querySelector('#navbar').offsetHeight;
-  }
-
-  calcHeight() {
-    return window.innerHeight - this.navbarHeight;
-  }
 }
