c5d1b9577dcf1c90875b6e4f31f8a2b9227e2bcd
Max Kofler <kofler.max.dev@gmail.com>
2023-02-03 09:21:34 +0100

    Added darkmode detection to config

--- src/app/shared/config/config.service.ts
+++ src/app/shared/config/config.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { takeWhile } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
   private config = {
-    branchAPIURL: 'https://api.acacialinux.org/'
+    branchAPIURL: 'https://api.acacialinux.org/',
+    isDarkMode: false
   }
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (isPlatformBrowser(this.platformId)){
+      this.checkForDarkMode();
+    }
+  }
+
+  checkForDarkMode() {
+    const mql = window.matchMedia('(prefers-color-scheme: dark)');
+    this.config.isDarkMode = mql.matches;
+    mql.addListener((e) => {
+      this.config.isDarkMode = e.matches;
+    });
+  }
 
   getBranchAPIURL() {
     return this.config.branchAPIURL;
+  }
+
+  isDarkMode(): boolean{
+    return this.config.isDarkMode;
   }
 }
