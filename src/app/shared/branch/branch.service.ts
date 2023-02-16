d061557400bc75fbb33a7aba1e256d8ff50c27e4
Max Kofler <kofler.max.dev@gmail.com>
2023-02-16 08:54:29 +0100

    Added authenticate() to BranchService

--- src/app/shared/branch/branch.service.ts
+++ src/app/shared/branch/branch.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ConfigService } from '../config/config.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+
+class BranchResponse{
+  status: string = ""
+  payload: any
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +21,25 @@ export class BranchService {
     return this.http.get(this.config.getBranchAPIURL() + "?get=" + get);
   }
 
+  authenticate(username: string, password: string){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      })
+    };
+
+    let req = "user=" + username + "&pass=" + password;
+
+    this.http.post(this.config.getBranchAPIURL() + "auth", req, httpOptions)
+      .pipe(map<any, BranchResponse>(data => data))
+      .subscribe(bresponse => {
+      if (bresponse.status != "SUCCESS"){
+        console.log("Authentication failed: " + bresponse.payload);
+        this.config.authKey = "";
+      } else {
+        console.log("Authentication ok!");
+        this.config.authKey = bresponse.payload;
+      }
+    });
+  }
 }
