d964850c812430e11c897c69332c35f5572b02e4
Max Kofler <kofler.max.dev@gmail.com>
2023-02-23 12:19:03 +0100

    EventService: Push DATA_REFRESH every 5 seconds

--- src/app/shared/event/event.service.ts
+++ src/app/shared/event/event.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 export enum EventType{
   EDITOR_SUBMIT,
@@ -15,7 +16,12 @@ export class EventService {
 
   public emitter: EventEmitter<EventType> = new EventEmitter<EventType>();
 
-  constructor() { }
+  constructor() {
+    const source = interval(5000);
+    source.subscribe(val => {
+        this.push(EventType.DATA_REFRESH);
+      });
+  }
 
   public push(type: EventType){
     this.emitter.emit(type);
