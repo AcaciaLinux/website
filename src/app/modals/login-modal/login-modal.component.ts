b110896703414a131ff50b8026d92e8bbf3b56d6
Max Kofler <kofler.max.dev@gmail.com>
2023-03-10 11:19:57 +0100

    LoginModal: Show toasts

--- src/app/modals/login-modal/login-modal.component.ts
+++ src/app/modals/login-modal/login-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BranchService } from 'src/app/shared/branch/branch.service';
+import { ToastService } from 'src/app/toasts-container/toast-service';
 
 @Component({
   selector: 'app-login-modal',
@@ -13,7 +14,7 @@ export class LoginModalComponent {
   public closeModal: string = "Close Modal!";
   @ViewChild('btnLogin', { static: false }) btnLogin: ElementRef<HTMLButtonElement> | undefined;
 
-  constructor(private modalService: NgbModal, public branchService: BranchService) {}
+  constructor(private modalService: NgbModal, public branchService: BranchService, private toasts: ToastService) {}
 
   public show(modalData: any){
     this.modalService.open(modalData, {ariaLabelledBy: 'modal-basic-title'});
@@ -22,10 +23,17 @@ export class LoginModalComponent {
   //Calls BranchService::authenticate and dismisses the dialog
   authenticate(username: string, password: string){
     //Just let the call go, subscribe it but do nothing...
-    this.branchService.authenticate(username, password).subscribe();
-
-    //And dismiss the dialog
-    this.modalService.dismissAll();
+    this.branchService.authenticate(username, password).subscribe(
+      res => {
+        if (res == ""){
+          this.toasts.s_ok("Logged in");
+          //And dismiss the dialog
+          this.modalService.dismissAll();
+        } else {
+          this.toasts.s_err("Login failed: " + res);
+        }
+      }
+    );
   }
 
   //Calls BranchService::logoff() to log off
