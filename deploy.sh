fcd50d900428b2653913ef1838f6cacb412bf8c1
Max Kofler <kofler.max.dev@gmail.com>
2023-03-11 09:27:45 +0100

    Added deploy.sh script for convenience

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Deployment url is needed!"
+    exit -1
+fi
+
+TARGET=$1
+echo "Deploying to $TARGET"
+
+ng build
+
+echo "Creating tar file..."
+tar cJpf website.tar.xz -C dist/website/ .
+
+echo "Transferring tar to $TARGET..."
+scp website.tar.xz $TARGET
+
+WORKDIR="$(ssh $TARGET pwd)"
+echo "File is available at $TARGET:$WORKDIR/website.tar.xz"
