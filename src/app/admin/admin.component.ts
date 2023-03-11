0c81930de2fa8cd426af4761471ceefa177096b3
Max Kofler <kofler.max.dev@gmail.com>
2023-03-11 09:02:02 +0100

    Admin: Add user creation

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BranchService } from '../shared/branch/branch.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  constructor(public branch: BranchService){
 
+  }
 }
