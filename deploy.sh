e02a3692c5664eb947cc10404c65a3fd764a54d2
Max Kofler <kofler.max.dev@gmail.com>
2023-03-11 10:06:03 +0100

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo "Creating tar file..."
 tar cJpf website.tar.xz -C dist/website/ .
 
 echo "Transferring tar to $TARGET..."
-scp website.tar.xz $TARGET
+scp website.tar.xz $TARGET:
 
 WORKDIR="$(ssh $TARGET pwd)"
 echo "File is available at $TARGET:$WORKDIR/website.tar.xz"
