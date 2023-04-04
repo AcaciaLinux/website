86949ab4a23f6465062280c684d89cb4a9c5d4f5
Max Kofler <kofler.max.dev@gmail.com>
2023-04-04 10:11:12 +0200

    LogViewModal: Toast if failed to view log

--- src/app/modals/log-view-modal/log-view-modal.component.ts
+++ src/app/modals/log-view-modal/log-view-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BranchService } from 'src/app/shared/branch/branch.service';
+import { ToastService } from 'src/app/toasts-container/toast-service';
 
 @Component({
   selector: 'app-log-view-modal',
@@ -13,18 +14,19 @@ export class LogViewModalComponent {
   public jobID: string = "";
   @ViewChild('modalData', { static: false }) modalData: ElementRef | undefined;
 
-  constructor(private modalService: NgbModal, private branch: BranchService) {
+  constructor(private modalService: NgbModal, private branch: BranchService, private toasts: ToastService) {
 
   }
 
   show(jobID: string){
     this.branch.getlog(jobID).subscribe(ok => {
       if (ok !== undefined){
-        console.log("OK: " + ok);
         this.jobID = jobID;
         this.log = "";
         ok.forEach(v => this.log += v + "<br>");
         this.modalService.open(this.modalData, {fullscreen: true, scrollable: true});
+      } else {
+        this.toasts.s_err("Log is not (yet) available!");
       }
     })
   }
