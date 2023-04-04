82d0e4890c235226d4b12c8c1dd6bfcd6f734f5d
Max Kofler <kofler.max.dev@gmail.com>
2023-04-04 13:38:01 +0200

    Revert "Job: Compatibility with new API"
    This reverts commit 1511da29c2a247af2b8daacf857e16854d81ba67.
    This is for compatibility with the old branch API for now

--- src/app/shared/classes/job.ts
+++ src/app/shared/classes/job.ts
@@ -1,5 +1,5 @@
 export class Job{
-	public job_name: string = ""
+	public build_pkg_name: string = ""
 	public job_id: string = ""
 	public requesting_client: string = ""
 	public job_status: string = ""
